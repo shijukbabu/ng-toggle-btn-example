39a78459a81ac18b91c6b9085d03776f5207195d
netscitus <shijukbabu@gmail.com>
2015-09-23 17:05:21 +0530

    Support for checked and unchecked values instead trueor false

--- app/scripts/ng-toggle-btn.js
+++ app/scripts/ng-toggle-btn.js
@@ -19,13 +19,15 @@ angular.module('ng-toggle.btn', [])
      ngChange: '&',
      onLabel: '@',
      offLabel: '@',
-     required: '@'
+     required: '@',
+     ngTrueValue:'@',
+     ngFalseValue:'@'
    },
    template:
        ' <div class="toggle-switch" ng-class="\'on\'+onType+ \' \' + \'off\'+offType"> ' +
        '		<span ng-if="offLabel" class="off-label" ng-bind="offLabel"></span> ' +
        ' 	<input ng-model="ngModel" id="{{name}}" name="{{name}}" type="checkbox" selected="ngModel" ng-disabled="isDisabled" ng-change="ngChange()" ' +
-       '			hidden="" ng-required="required"><label for="{{name}}" ' +
+       '			hidden="" ng-true-value="{{ngTrueValue? ngTrueValue:true}}" ng-false-value="{{ngFalseValue? ngFalseValue:false}}" ng-required="required"><label for="{{name}}" ' +
        '			class="toggle-knob"></label> ' +
        '		<span ng-if="onLabel" class="on-label" ng-bind="onLabel"></span> ' +
        '	</div> '
